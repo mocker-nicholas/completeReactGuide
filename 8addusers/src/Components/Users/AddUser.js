5c9d910089a85cafb878c43efb55caa7a0ddade1
Nick Orth <nicholas.r.orth@gmail.com>
2022-04-20 18:52:42 -0500

    Add user and age validation to adduser project

--- 8addusers/src/Components/Users/AddUser.js
+++ 8addusers/src/Components/Users/AddUser.js
@@ -1,20 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./AddUser.module.css";
 import Card from "../Ui/Card.js";
 import Button from "../Ui/Button.js";
 
 const AddUser = () => {
+  const [enteredUsername, setEnteredUsername] = useState("");
+  const [enteredAge, setEnteredAge] = useState("");
+
+  const usernameChangeHandler = (e) => {
+    setEnteredUsername(e.target.value);
+  };
+
+  const ageChangeHandler = (e) => {
+    setEnteredAge(e.target.value);
+  };
+
   const addUserHandler = (e) => {
     e.preventDefault();
+    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+      return;
+    }
+    // Remember, everything from the dom is a string. We can force a conversion to a number with  a + character
+    if (+enteredAge < 1) {
+      return;
+    }
+    console.log(enteredUsername, enteredAge);
+    setEnteredUsername("");
+    setEnteredAge("");
   };
 
   return (
     <Card className={styles.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
-        <input id="username" type="text"></input>
+        <input
+          id="username"
+          type="text"
+          onChange={usernameChangeHandler}
+          value={enteredUsername}
+        ></input>
         <label htmlFor="age">Age (years)</label>
-        <input id="age" type="number"></input>
+        <input
+          id="age"
+          type="number"
+          onChange={ageChangeHandler}
+          value={enteredAge}
+        ></input>
         <Button type="submit">Add User</Button>
       </form>
     </Card>
