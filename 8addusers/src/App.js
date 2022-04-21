a852f798c284f67253cb52ad96012dff17590c56
Nick Orth <nicholas.r.orth@gmail.com>
2022-04-20 20:34:22 -0500

    Add userslist component to addusers app

--- 8addusers/src/App.js
+++ 8addusers/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import AddUser from "./Components/Users/AddUser.js";
+import UsersList from "./Components/Users/UsersList.js";
 
 function App() {
   return (
     <div>
       <AddUser />
+      <UsersList users={[]} />
     </div>
   );
 }
