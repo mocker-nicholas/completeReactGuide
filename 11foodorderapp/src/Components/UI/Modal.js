95a97811fc50d88b1f41f98562ed9d04df885b32
Nick Orth <nicholas.r.orth@gmail.com>
2022-04-30 16:43:17 -0500

    Add cart state

---
+++ 11foodorderapp/src/Components/UI/Modal.js
@@ -0,0 +1,32 @@
+import React from "react";
+import styles from "./Modal.module.css";
+import ReactDOM from "react-dom";
+
+const Backdrop = (props) => {
+  return <div className={styles.backdrop} onClick={props.onHideCart}></div>;
+};
+
+const ModalOverlay = (props) => {
+  return (
+    <div className={styles.modal}>
+      <div className={styles.content}>{props.children}</div>
+    </div>
+  );
+};
+
+const Modal = (props) => {
+  return (
+    <React.Fragment>
+      {ReactDOM.createPortal(
+        <Backdrop onHideCart={props.onHideCart} />,
+        document.getElementById("overlays")
+      )}
+      {ReactDOM.createPortal(
+        <ModalOverlay>{props.children}</ModalOverlay>,
+        document.getElementById("overlays")
+      )}
+    </React.Fragment>
+  );
+};
+
+export default Modal;
