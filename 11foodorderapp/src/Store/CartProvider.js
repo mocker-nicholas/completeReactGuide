1ea7d1248036dbc3a0de3182198561908ccaa033
Nick Orth <nicholas.r.orth@gmail.com>
2022-05-01 07:54:57 -0500

    Add remove and add logic to cart buttons

--- 11foodorderapp/src/Store/CartProvider.js
+++ 11foodorderapp/src/Store/CartProvider.js
@@ -30,12 +30,40 @@ const cartReducer = (state, action) => {
       updatedItems = state.items.concat(action.item);
     }
 
-    // const updatedItems = state.items.concat(action.item);
     return {
       items: updatedItems,
       totalAmount: updatedTotalAmount,
     };
   }
+
+  if (action.type === "REMOVE_ITEM") {
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.id
+    );
+
+    const existingCartItem = state.items[existingCartItemIndex];
+    const updatedTotalAmount = state.totalAmount - existingCartItem.price;
+
+    let updatedItems;
+    if (existingCartItem.amount === 1) {
+      updatedItems = state.items.filter((item) => item.id !== action.id);
+    } else {
+      console.log(existingCartItem.amount);
+      const updatedItem = {
+        ...existingCartItem,
+        amount: existingCartItem.amount - 1,
+      };
+      console.log(updatedItem);
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
+    }
+
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount,
+    };
+  }
+
   return defaultCartState;
 };
 
